44ddb576023f39e61816ef5371da048436ed5b31
albinecs1-lang <albinecs1@gmail.com>
2025-09-24 14:38:56 +0530

    Create new-file.py

---
+++ SI EC ALBIN/new-file.py
@@ -0,0 +1,16 @@
+#finding power,factorial,squre root of a number 
+
+#importing requerd module
+import math as mt
+
+#power
+number = 1
+powering = mt.pow(number , number)
+
+#factorial
+factorial = mt.factorial(number)
+
+#squre root
+squre_root = mt.sqrt(number)
+
+print(f'the power at {number} is {powering} the factorial is {factorial} the squre root is {squre_root}')
