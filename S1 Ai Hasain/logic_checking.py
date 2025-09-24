6260f03ae6c40163953d993df53aad9b79759e6f
albinecs1-lang <albinecs1@gmail.com>
2025-09-24 15:14:58 +0530

    Create logic_checking.py

---
+++ S1 Ai Hasain/logic_checking.py
@@ -0,0 +1,9 @@
+#finding if the logic is correct using <,> and more....
+number = 10
+number_2 = 20
+
+#checking wether the first number is grater than the second
+print(f'wether the number 1 is grater than number 2 {number > number_2}')
+
+#checking the reverce.
+print(f'wether the number 2 is gater than number 1 {number < number_2}')
