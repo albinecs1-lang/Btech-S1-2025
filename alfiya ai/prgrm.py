8a39cd50e1233c16c7fe6fc901d8ad1b26105cb5
albinecs1-lang <albinecs1@gmail.com>
2025-09-24 14:50:24 +0530

    Add files via upload

---
+++ alfiya ai/prgrm.py
@@ -0,0 +1,4 @@
+a=10
+mod=(a%2)
+print("the number is even")
+print(mod==0)
---
+++ alfiya ai/project1.py
@@ -0,0 +1,7 @@
+a= "musaliar "
+b= "college "
+c= "of "
+d= "engineering "
+e= "and "
+f= "technology "
+print(a+b+c+d+e+f)
