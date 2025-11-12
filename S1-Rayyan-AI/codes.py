1f96389520bf28e54a8e1224e7ef2df8f8d7c94b
albinecs1-lang <albinecs1@gmail.com>
2025-11-12 14:37:30 +0530

    Update codes.py

--- S1-Rayyan-AI/codes.py
+++ S1-Rayyan-AI/codes.py
@@ -13,19 +13,21 @@ string_value = "hellow who how why by"
 c = string_value.split()
 print(len(c))
 
+
 #to reverce a num
 string = "asddsa"
 rever = string[::-1]
-
 if rever == string:
   print("yes it is")
 else:
   print("not")
 
 
+
 #slicing a word
 slised = "malayalam"
 print(slised[0:4])
+
 
 #reversing a string
 reverserr = "stringgg"
